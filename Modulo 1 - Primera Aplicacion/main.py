bd2980584c322f6fab4ffb930e23b03885070594
mijangos183438 <50309437+mijangos183438@users.noreply.github.com>
2022-02-07 21:10:34 -0600

    Todas las actividades realizadas

---
+++ Modulo 1 - Primera Aplicacion/main.py
@@ -0,0 +1,8 @@
+#Saber la hora
+from datetime import date
+print("El día de hoy es: ", date.today())
+
+#Conversor de unidades
+parsecs = 11
+lightyears = parsecs * 3.26156
+print(parsecs , " parsecs son ", lightyears)
